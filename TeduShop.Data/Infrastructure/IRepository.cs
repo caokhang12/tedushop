e0a95ee33bab14000582f5ef7c275bc03834f43a
khang <khangcaopham2003@gmail.com>
2023-10-26 22:31:09 +0700

    hoc do bai 6 26:43

---
+++ TeduShop.Data/Infrastructure/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Data.Infrastructure
+{
+    public interface IRepository<T> where T : class
+    {
+        //mark an emty as new
+        void Add(T entity);
+
+    }
+}
